6468afd37f38a284833ef8f28a399bec965731d2
nguyenngocha <dodaihoc.abvk@gmail.com>
2016-06-17 10:03:33 +0700

    relationship

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,8 +1,26 @@
 class RelationshipsController < ApplicationController 
-  load_and_authorize_resource :user
+  load_and_authorize_resource :user, only: [:index]
   
   def index
     @title = params[:action_type]
     @users = @user.send(params[:action_type]).page params[:page]
+  end
+
+  def create
+    @user = User.find params[:followed_id]
+    current_user.follow @user
+    respond_to do |format|
+      format.html{redirect_to @user}
+      format.js
+    end
+  end
+
+  def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow @user
+    respond_to do |format|
+      format.html{redirect_to @user}
+      format.js
+    end
   end
 end
