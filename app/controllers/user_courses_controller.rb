399827fb6e0dfff114672374cf49a9fee54ac2b0
nguyenngocha <dodaihoc.abvk@gmail.com>
2016-06-22 16:49:33 +0700

    view my activities in a course

--- app/controllers/user_courses_controller.rb
+++ app/controllers/user_courses_controller.rb
@@ -7,6 +7,16 @@ class UserCoursesController < ApplicationController
   def show
     @user_courses = @user_course.course.user_courses.page(params[:page]).
       per Settings.courses.per_page
-    @activities = PublicActivity::Activity.course @user_course.course
+    type_activity = params[:action_type]
+    if type_activity == Settings.activity.my_activities
+      @activities = PublicActivity::Activity
+        .user(current_user).course(@user_course.course)
+          .page(params[:page]).per Settings.activity.per_page
+      @bottom = Settings.activity.all_activities
+    else
+      @activities = PublicActivity::Activity.course(@user_course.course)
+        .page(params[:page]).per Settings.activity.per_page
+      @bottom = Settings.activity.my_activities
+    end
   end
 end
