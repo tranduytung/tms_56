21271debc931292b2c04eb115ff88c985b3c926b
tranduytung <tranduytung1994@gmail.com>
2016-06-23 09:10:30 +0700

    create trainee-subject while add subject for course

--- app/models/course_subject.rb
+++ app/models/course_subject.rb
@@ -13,6 +13,8 @@ class CourseSubject < ActiveRecord::Base
   tracked
 
   after_update :activity
+  after_create :create_trainee_subject_by_subject
+
   def activity
     if started?
       create_activity key: I18n.t("activity.subject.started"), recipient: course
@@ -23,6 +25,21 @@ class CourseSubject < ActiveRecord::Base
       create_activity key: I18n.t("activity.subject.finished"), recipient: course
       course.users.each do |user|
         create_activity key: I18n.t("activity.subject.finished"), owner: user
+      end
+    end
+  end
+
+  private
+  def create_trainee_subject_by_subject
+    TraineeSubject.transaction do
+      begin
+        course.user_courses.each do |user_course|
+          @trainee_subjects = user_course.trainee_subjects.
+            create! user: user_course.user, subject: subject,
+            course_subject: self
+        end
+      rescue
+        raise ActiveRecord::Rollback
       end
     end
   end
