83ff9c39673bc063775c76ac59ed9bc458bebeaf
tranduytung <tranduytung1994@gmail.com>
2016-06-24 16:31:00 +0700

    fix gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
+
   root "static_pages#home"
 
   get "home" => "static_pages#home"
