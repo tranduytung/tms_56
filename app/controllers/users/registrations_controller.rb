6a88ac753924a6f771c5ec0ca4df770dee73d197
tranduytung <tranduytung1994@gmail.com>
2016-06-24 14:40:03 +0700

    config gem devise for user

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,30 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_filter :configure_sign_up_params, only: [:create]
+  before_filter :configure_account_update_params, only: [:update]
+
+  def new
+    super
+  end
+
+  def create
+    super
+  end
+
+  def edit
+    super
+  end
+
+  def update
+    super
+  end
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit :email,
+      :name, :password, :password_confirmation}
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.for(:account_update) {|u| u.permit :email,
+      :name, :current_password, :password, :password_confirmation}
+  end
+end
