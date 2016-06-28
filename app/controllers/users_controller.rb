4dc68ebc1017be4211d4f4d711854a0830c8444b
nguyenngocha <dodaihoc.abvk@gmail.com>
2016-06-28 14:17:13 +0700

    fix course activity

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,8 @@ class UsersController < ApplicationController
   load_and_authorize_resource
   
   def show
+    @activities = PublicActivity::Activity
+      .user(@user)
+      .page(params[:page]).per Settings.activity.per_page
   end
 end
